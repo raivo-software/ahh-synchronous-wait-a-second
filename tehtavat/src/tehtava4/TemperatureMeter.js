ca1791c7d00be053ddcbb44c33f47d80072542da
Teemu Salminen <teemujsa@gmail.com>
2019-09-11 02:17:22 +0300

    Add more descriptions for assignment 4.

--- tehtavat/src/tehtava4/TemperatureMeter.js
+++ tehtavat/src/tehtava4/TemperatureMeter.js
@@ -1,6 +1,7 @@
 /*
 4. Toteuta lämpömittari React-komponenttina (TemperatureMeter)
 - Komponenttiin kuuluu lämpömittari-ikoni, joka on annettu valmiiksi toteutettuna
+ - Lämpömittari-ikonille annetaan parametrina lämpötila, joka on normalisoitu 0...1 välille niin, että -30 asteen lämpötilat ja pienemmät ovat 0, ja +30 asteen lämpötilat ja suuremmat ovat 1. Välillä olevat lämpötilat asettuvat lineaarisesti 0:n ja 1:n väliin.
 - Komponentti hakee fetch -funktiolla localhostista lämpötilan, ja antaa sen parametrina lämpötila-ikonille. Ikonin värin pitäisi muuttua lämpötilan mukaan.
     - Haetun JSON-objektin oletetaan sisältävän kenttä nimeltä "temperature", joka sisältää lämpötilan numero-muotoisena
 - Komponentti tulostaa lämpötilan myös tekstikenttään
